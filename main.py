c947424f7da9c367e7faec3aaf7b4c7a91019946
William Bell <wjbell97@gmail.com>
2022-03-19 18:09:18 -0400

    Now sends a leaderboard with average number of guesses and total wordles played

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from gets import get_all_wordle_messages
 from analysis import extract_to_dataframe
-from messaging import send_leaderboard_as_huey
+from messaging import send_average_guesses_and_total_wordles_leaderboard
 
 if __name__ == '__main__':
+    # Scrape the GroupMe messages
     msgs = get_all_wordle_messages()
 
+    # Parse the messages to get the Wordle data, and keep it in a pandas dataframe for analysis
     df = extract_to_dataframe(msgs)
-    print(df)
 
-    leader_board = df.mean(axis=1).sort_values()
-    print(leader_board)
+    # Get the average number of guesses each player has used
+    average_guesses = df.mean(axis=1)
+    sort_ind = average_guesses.argsort().values
+    average_guesses_leaderboard = average_guesses.iloc[sort_ind]
+    print(average_guesses_leaderboard)
+    
+    # Count the number of Wordles each person has played
+    played_wordle = df > 0
+    total_wordles_leaderboard = played_wordle.sum(axis=1)
+    total_wordles_leaderboard = total_wordles_leaderboard.iloc[sort_ind]
+    print(total_wordles_leaderboard)
 
-    send_leaderboard_as_huey(leader_board)
+    # Send a message with the leaderboards
+    send_average_guesses_and_total_wordles_leaderboard(average_guesses_leaderboard, total_wordles_leaderboard)
